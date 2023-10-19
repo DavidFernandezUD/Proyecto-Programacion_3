085edf3a47f0e7c6a3e139652779c6a64c904942
David Fernández Fernández <david.f@opendeusto.es>
2023-10-19 18:08:07 +0200

    removed prop collision (temporary)

--- src/main/CollisionChecker.java
+++ src/main/CollisionChecker.java
@@ -33,61 +33,53 @@ public class CollisionChecker {
             case "up":
                 topRow = (collisionTopBound - entity.speed) / gamePanel.tileSize;
 
-                tileNum1 = gamePanel.tileManager.level1TileNum[topRow][leftCol] *
-                           gamePanel.tileManager.level2TileNum[topRow][leftCol] *
-                           gamePanel.tileManager.propTileNum[topRow][leftCol];
+                tileNum1 = gamePanel.tileManager.level1TileNum[topRow][leftCol] +
+                           gamePanel.tileManager.level2TileNum[topRow][leftCol];
 
-                tileNum2 = gamePanel.tileManager.level1TileNum[topRow][rightCol] *
-                           gamePanel.tileManager.level2TileNum[topRow][rightCol] *
-                           gamePanel.tileManager.propTileNum[topRow][rightCol];
+                tileNum2 = gamePanel.tileManager.level1TileNum[topRow][rightCol] +
+                           gamePanel.tileManager.level2TileNum[topRow][rightCol];
                 
                 // Only checking for collisions if a solid tile exist at those positions
-                if(tileNum1 != -1 || tileNum2 != -1) {
+                if(tileNum1 != -2 || tileNum2 != -2) {
                     entity.collisionOn = true;
                 }
                 break;
             case "down":
                 bottomRow = (collisionBottomBound + entity.speed) / gamePanel.tileSize;
 
-                tileNum1 = gamePanel.tileManager.level1TileNum[bottomRow][leftCol] *
-                           gamePanel.tileManager.level2TileNum[bottomRow][leftCol] * 
-                           gamePanel.tileManager.propTileNum[bottomRow][leftCol];
+                tileNum1 = gamePanel.tileManager.level1TileNum[bottomRow][leftCol] +
+                           gamePanel.tileManager.level2TileNum[bottomRow][leftCol];
 
-                tileNum2 = gamePanel.tileManager.level1TileNum[bottomRow][rightCol] * 
-                           gamePanel.tileManager.level2TileNum[bottomRow][rightCol] *
-                           gamePanel.tileManager.propTileNum[bottomRow][rightCol];
+                tileNum2 = gamePanel.tileManager.level1TileNum[bottomRow][rightCol] + 
+                           gamePanel.tileManager.level2TileNum[bottomRow][rightCol];
 
-                if(tileNum1 != -1 || tileNum2 != -1) {
+                if(tileNum1 != -2 || tileNum2 != -2) {
                     entity.collisionOn = true;
                 }
                 break;
             case "left":
                 leftCol = (collisionLeftBound - entity.speed) / gamePanel.tileSize;
 
-                tileNum1 = gamePanel.tileManager.level1TileNum[topRow][leftCol] *
-                           gamePanel.tileManager.level2TileNum[topRow][leftCol] * 
-                           gamePanel.tileManager.propTileNum[topRow][leftCol];
+                tileNum1 = gamePanel.tileManager.level1TileNum[topRow][leftCol] +
+                           gamePanel.tileManager.level2TileNum[topRow][leftCol];
 
-                tileNum2 = gamePanel.tileManager.level1TileNum[bottomRow][leftCol] * 
-                           gamePanel.tileManager.level2TileNum[bottomRow][leftCol] * 
-                           gamePanel.tileManager.propTileNum[bottomRow][leftCol];
+                tileNum2 = gamePanel.tileManager.level1TileNum[bottomRow][leftCol] + 
+                           gamePanel.tileManager.level2TileNum[bottomRow][leftCol];
 
-                if(tileNum1 != -1 || tileNum2 != -1) {
+                if(tileNum1 != -2 || tileNum2 != -2) {
                     entity.collisionOn = true;
                 }
                 break;
             case "right":
                 rightCol = (collisionRightBound + entity.speed) / gamePanel.tileSize;
 
-                tileNum1 = gamePanel.tileManager.level1TileNum[topRow][rightCol] *
-                           gamePanel.tileManager.level2TileNum[topRow][rightCol] * 
-                           gamePanel.tileManager.propTileNum[topRow][rightCol];
+                tileNum1 = gamePanel.tileManager.level1TileNum[topRow][rightCol] +
+                           gamePanel.tileManager.level2TileNum[topRow][rightCol];
 
-                tileNum2 = gamePanel.tileManager.level1TileNum[bottomRow][rightCol] * 
-                           gamePanel.tileManager.level2TileNum[bottomRow][rightCol] * 
-                           gamePanel.tileManager.propTileNum[bottomRow][rightCol];
+                tileNum2 = gamePanel.tileManager.level1TileNum[bottomRow][rightCol] + 
+                           gamePanel.tileManager.level2TileNum[bottomRow][rightCol];
 
-                if(tileNum1 != -1 || tileNum2 != -1) {
+                if(tileNum1 != -2 || tileNum2 != -2) {
                     entity.collisionOn = true;
                 }
                 break;
