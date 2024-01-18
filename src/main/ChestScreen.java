d8dee3bbd97b9f71ed3e4916cc9fbe875927ed8a
marcosmartinezud <marcos.martinez@opendeusto.es>
2024-01-18 13:02:47 +0100

    Close button experimenting

--- src/main/ChestScreen.java
+++ src/main/ChestScreen.java
@@ -13,8 +13,11 @@ import main.items.ITEM_purplePotion;
 import main.items.ITEM_redPotion;
 import main.items.SuperItem;
 
-/** Drawable chest inventory GUI component.
- * @author marcos.martinez@opendeusto.es*/
+/**
+ * Drawable chest inventory GUI component.
+ * 
+ * @author marcos.martinez@opendeusto.es
+ */
 public class ChestScreen implements Drawable {
 
 	private boolean upToggled = false;
@@ -24,6 +27,7 @@ public class ChestScreen implements Drawable {
 	private boolean lToggled = false;
 	private boolean cursorOnChest = true;
 	private boolean cursorOnInventory = false;
+	private boolean onCloseButton = false;
 
 	// SETTINGS
 	private Font optionFont;
@@ -36,7 +40,7 @@ public class ChestScreen implements Drawable {
 	// OPTIONS
 	public GamePanel gamePanel;
 
-	/** Constructor for ChestScreen.*/
+	/** Constructor for ChestScreen. */
 	ChestScreen(GamePanel gamePanel) {
 		this.gamePanel = gamePanel;
 
@@ -48,51 +52,71 @@ public class ChestScreen implements Drawable {
 		highlightColor = Color.YELLOW;
 	}
 
-	/** Updates the inventory in response to user input,
-	 * and triggers sounds when necessary.*/
+	/**
+	 * Updates the inventory in response to user input, and triggers sounds when
+	 * necessary.
+	 */
 	public void update() {
+		// TODO: Fix close button
 		if (gamePanel.keyHandler.isKeyToggled(KeyEvent.VK_W) != upToggled) {
 			upToggled = !upToggled;
-			if (slotRow != 0) {
-				slotRow--;
-				gamePanel.playSound(1);
+			if (!onCloseButton) {
+				if (slotRow != 0) {
+					slotRow--;
+					gamePanel.playSound(1);
+				}
+				if (cursorOnChest) {
+					if (slotRow == 0 && gamePanel.keyHandler.isKeyToggled(KeyEvent.VK_W)) {
+						onCloseButton = true;
+					}
+				}
 			}
-
+			System.out.println(slotRow);
 		}
 		if (gamePanel.keyHandler.isKeyToggled(KeyEvent.VK_A) != leftToggled) {
 			leftToggled = !leftToggled;
-			if (slotCol == 6) {
-				slotCol -= 1;
-				cursorOnChest = true;
-				cursorOnInventory = false;
-				System.out.println(cursorOnInventory);
+			if (!onCloseButton) {
+				if (slotCol == 6) {
+					slotCol -= 1;
+					cursorOnChest = true;
+					cursorOnInventory = false;
+					System.out.println(cursorOnInventory);
+				}
+				if (slotCol != 0) {
+					slotCol--;
+					gamePanel.playSound(1);
+				}
 			}
-			if (slotCol != 0) {
-				slotCol--;
-				gamePanel.playSound(1);
-			}
-
 		}
 		if (gamePanel.keyHandler.isKeyToggled(KeyEvent.VK_S) != downToggled) {
 			downToggled = !downToggled;
-			if (slotRow != 3) {
-				slotRow++;
-				gamePanel.playSound(1);
+			if (!onCloseButton) {
+				if (slotRow != 3) {
+					slotRow++;
+					gamePanel.playSound(1);
+				}
 			}
+			if (onCloseButton) {
+				onCloseButton = false;
+				slotRow = 0;
+			}
+
 		}
 		if (gamePanel.keyHandler.isKeyToggled(KeyEvent.VK_D) != rightToggled) {
 			rightToggled = !rightToggled;
-			if (slotCol == 4) {
-				slotCol += 1;
-				cursorOnInventory = true;
-				cursorOnChest = false;
-				System.out.println(cursorOnInventory);
-			}		
-			if (slotCol != 10) {
-				slotCol++;
-				gamePanel.playSound(1);
+			if (!onCloseButton) {
+				if (slotCol == 4) {
+					slotCol += 1;
+					cursorOnInventory = true;
+					cursorOnChest = false;
+					System.out.println(cursorOnInventory);
+				}
+				if (slotCol != 10) {
+					slotCol++;
+					gamePanel.playSound(1);
+				}
 			}
-		}	
+		}
 		// Inventory cannot be opened while chest screen
 		if (gamePanel.chestState) {
 			gamePanel.inventoryState = false;
@@ -123,7 +147,7 @@ public class ChestScreen implements Drawable {
 				selectItemFromInventory();
 			}
 		}
-	}	
+	}
 
 	private void selectItemFromInventory() {
 		ASSET_Chest chest = null;
@@ -135,7 +159,7 @@ public class ChestScreen implements Drawable {
 					}
 				}
 			}
-		}	
+		}
 		int itemIndex = getItemIndexOnSlotInventory();
 		if (itemIndex < gamePanel.player.inventory.size()) {
 			SuperItem selectedItem = gamePanel.player.inventory.get(itemIndex);
@@ -145,7 +169,7 @@ public class ChestScreen implements Drawable {
 				System.out.println(selectedItem.toString());
 			}
 		}
-		
+
 	}
 
 	/** Selects item from the inventory. */
@@ -159,7 +183,7 @@ public class ChestScreen implements Drawable {
 					}
 				}
 			}
-		}			
+		}
 		int itemIndex = getItemIndexOnSlotChest();
 		if (itemIndex < chest.chestItems.size()) {
 			SuperItem selectedItem = chest.chestItems.get(itemIndex);
@@ -171,8 +195,11 @@ public class ChestScreen implements Drawable {
 		}
 	}
 
-	/** Draws a graphical representation of the chests inventory.
-	 * @param g2 Graphics2D object to draw the inventory to.*/
+	/**
+	 * Draws a graphical representation of the chests inventory.
+	 * 
+	 * @param g2 Graphics2D object to draw the inventory to.
+	 */
 	@Override
 	public void draw(Graphics2D g2) {
 		// FRAME
@@ -188,7 +215,7 @@ public class ChestScreen implements Drawable {
 		final int slotYStart = frameY + 20;
 		int slotX = slotXStart;
 		int slotY = slotYStart;
-		
+
 		// DRAW PLAYER'S ITEMS
 		ASSET_Chest chest = null;
 		for (SuperAsset sa : gamePanel.assets) {
@@ -200,12 +227,12 @@ public class ChestScreen implements Drawable {
 				}
 			}
 		}
-		
+
 		for (int i = 0; i < chest.chestItems.size(); i++) {
-			g2.drawImage(chest.chestItems.get(i).image, slotX+8, slotY+8, null);
-			
+			g2.drawImage(chest.chestItems.get(i).image, slotX + 8, slotY + 8, null);
+
 			slotX += gamePanel.tileSize;
-			
+
 			if (i == 4 || i == 9 || i == 14) {
 				slotX = slotXStart;
 				slotY += gamePanel.tileSize;
@@ -219,68 +246,82 @@ public class ChestScreen implements Drawable {
 		int cursorHeight = gamePanel.tileSize;
 
 		// DRAW CURSOR
-		g2.setColor(fontColor);
-		g2.setStroke(new BasicStroke(3));
-		g2.drawRoundRect(cursorX, cursorY, cursorWidth, cursorHeight, 10, 10);
-		
+		if (!onCloseButton) {
+			g2.setColor(fontColor);
+			g2.setStroke(new BasicStroke(3));
+			g2.drawRoundRect(cursorX, cursorY, cursorWidth, cursorHeight, 10, 10);
+		}
+
 		// DESCRIPTION FRAME
 		int dFrameX = frameX;
 		int dFrameY = frameY + frameHeight;
 		int dFrameWidth = frameWidth;
-		int dFrameHeight = gamePanel.tileSize*3;
-		
+		int dFrameHeight = gamePanel.tileSize * 3;
+
 		// DRAW DESCRIPTION TEXT
 		int textX = dFrameX + 20;
 		int textY = dFrameY + gamePanel.tileSize;
 		g2.setFont(optionFont);
-		
+
 		if (cursorOnChest) {
 			int itemIndex = getItemIndexOnSlotChest();
-			
+
 			if (itemIndex < chest.chestItems.size()) {
-				
+
 				drawSubWindow(dFrameX, dFrameY, dFrameWidth, dFrameHeight, g2);
-				
-				for (String line: chest.chestItems.get(itemIndex).description.split("\n")) {
+
+				for (String line : chest.chestItems.get(itemIndex).description.split("\n")) {
 					g2.drawString(line, textX, textY);
 					textY += 32;
-				}			
+				}
 			}
 		}
-		
+
 		// CLOSING FRAME
 		int cFrameX = gamePanel.tileSize * 3;
 		int cFrameY = gamePanel.tileSize * 2;
 		int cFrameWidth = gamePanel.tileSize * 2;
 		int cFrameHeight = gamePanel.tileSize * 1;
 		drawSubWindow(cFrameX, cFrameY, cFrameWidth, cFrameHeight, g2);
-		
-		g2.drawString("Close", cFrameX + 20, cFrameY + 40);	
+
+		g2.drawString("Close", cFrameX + 20, cFrameY + 40);
+
+		if (onCloseButton) {
+			g2.drawRoundRect(cFrameX + 18, cFrameY + 18, cFrameWidth - 40, cFrameHeight - 40, 10, 10);
+		}
 	}
-	
-	/** Helper method that flattens row col indexes
-	 * into a single index from inventory.
-	 * @return Flattened index.*/
+
+	/**
+	 * Helper method that flattens row col indexes into a single index from
+	 * inventory.
+	 * 
+	 * @return Flattened index.
+	 */
 	private int getItemIndexOnSlotInventory() {
-		int itemIndex = slotCol-6 + (slotRow*5);
+		int itemIndex = slotCol - 6 + (slotRow * 5);
 		return itemIndex;
 	}
 
-	/** Helper method that flattens row col indexes
-	 * into a single index from chest.
-	 * @return Flattened index.*/
+	/**
+	 * Helper method that flattens row col indexes into a single index from chest.
+	 * 
+	 * @return Flattened index.
+	 */
 	private int getItemIndexOnSlotChest() {
-		int itemIndex = slotCol + (slotRow*5);
+		int itemIndex = slotCol + (slotRow * 5);
 		return itemIndex;
 	}
 
-	/** Helper method that draws the main frame of the inventory,
-	 * where the item slots will then be drawn on top of.
+	/**
+	 * Helper method that draws the main frame of the inventory, where the item
+	 * slots will then be drawn on top of.
+	 * 
 	 * @param x      The x coordinate of the inventory.
-	 * @param y 	 The y coordinate of the inventory.
+	 * @param y      The y coordinate of the inventory.
 	 * @param width  Width of the inventory frame.
 	 * @param height Height of the inventory frame.
-	 * @param g2	 Graphics2D where the frame will be drawn into.*/
+	 * @param g2     Graphics2D where the frame will be drawn into.
+	 */
 	private void drawSubWindow(int x, int y, int width, int height, Graphics2D g2) {
 		Color c = new Color(0, 0, 0, 210);
 		g2.setColor(c);
@@ -292,9 +333,12 @@ public class ChestScreen implements Drawable {
 		g2.drawRoundRect(x + 5, y + 5, width - 10, height - 10, 25, 25);
 
 	}
-	
-	/** Draws the inventory window in a given Graphics2D object.
-	 * @param g2 Graphisc2D object where the inventory window will be drawn into.*/
+
+	/**
+	 * Draws the inventory window in a given Graphics2D object.
+	 * 
+	 * @param g2 Graphisc2D object where the inventory window will be drawn into.
+	 */
 	public void drawInventory(Graphics2D g2) {
 
 		// FRAME
@@ -309,19 +353,19 @@ public class ChestScreen implements Drawable {
 		final int slotYStart = frameY + 20;
 		int slotX = slotXStart;
 		int slotY = slotYStart;
-		
+
 		// DRAW PLAYER'S ITEMS
 		for (int i = 0; i < gamePanel.player.inventory.size(); i++) {
-			g2.drawImage(gamePanel.player.inventory.get(i).image, slotX+8, slotY+8, null);
-			
+			g2.drawImage(gamePanel.player.inventory.get(i).image, slotX + 8, slotY + 8, null);
+
 			slotX += gamePanel.tileSize;
-			
+
 			if (i == 4 || i == 9 || i == 14) {
 				slotX = slotXStart;
 				slotY += gamePanel.tileSize;
 			}
 		}
-		
+
 		// WEAPONS FRAME
 		int wFrameX = gamePanel.tileSize * 9;
 		int wFrameY = gamePanel.tileSize;
@@ -334,39 +378,39 @@ public class ChestScreen implements Drawable {
 		final int wSlotYStart = wFrameY + 20;
 		int wSlotX = wSlotXStart;
 		int wSlotY = wSlotYStart;
-		
+
 		// DRAW PLAYER'S WEAPONS
 		if (gamePanel.player.weapons[0] != null) {
-			g2.drawImage(gamePanel.player.weapons[0].image, wSlotX+8, wSlotY+8, null);
+			g2.drawImage(gamePanel.player.weapons[0].image, wSlotX + 8, wSlotY + 8, null);
 		}
 		if (gamePanel.player.weapons[1] != null) {
-			g2.drawImage(gamePanel.player.weapons[1].image, wSlotX+8 + gamePanel.tileSize, wSlotY+8, null);
+			g2.drawImage(gamePanel.player.weapons[1].image, wSlotX + 8 + gamePanel.tileSize, wSlotY + 8, null);
 		}
 		if (gamePanel.player.weapons[2] != null) {
-			g2.drawImage(gamePanel.player.weapons[2].image, wSlotX+8 + 2*gamePanel.tileSize, wSlotY+8, null);
+			g2.drawImage(gamePanel.player.weapons[2].image, wSlotX + 8 + 2 * gamePanel.tileSize, wSlotY + 8, null);
 		}
 
 		// DESCRIPTION FRAME
 		int dFrameX = frameX;
 		int dFrameY = frameY + frameHeight;
 		int dFrameWidth = frameWidth;
-		int dFrameHeight = gamePanel.tileSize*3;
-		
+		int dFrameHeight = gamePanel.tileSize * 3;
+
 		// DRAW DESCRIPTION TEXT
 		int textX = dFrameX + 20;
-		int textY = dFrameY + gamePanel.tileSize;	
+		int textY = dFrameY + gamePanel.tileSize;
 		g2.setFont(optionFont);
 		if (cursorOnInventory) {
 			int itemIndex = getItemIndexOnSlotInventory();
-			
+
 			if (itemIndex < gamePanel.player.inventory.size()) {
-				
+
 				drawSubWindow(dFrameX, dFrameY, dFrameWidth, dFrameHeight, g2);
-				
-				for (String line: gamePanel.player.inventory.get(itemIndex).description.split("\n")) {
+
+				for (String line : gamePanel.player.inventory.get(itemIndex).description.split("\n")) {
 					g2.drawString(line, textX, textY);
 					textY += 32;
-				}			
+				}
 			}
 		}
 	}
